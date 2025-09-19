9a7c690770f3cb3d2d67b1d55cff515b91fe0805
NotBryan <104746072+NotBryan@users.noreply.github.com>
2025-09-19 11:45:21 +0930

    Implementing debug functions in clockmmu

--- clockmmu.py
+++ clockmmu.py
@@ -4,14 +4,21 @@ from mmu import MMU
 class ClockMMU(MMU):
     def __init__(self, frames):
         # TODO: Constructor logic for EscMMU
+        
+        self.disk_reads = 0
+        self.disk_writes = 0
+        self.page_faults = 0
+
         pass
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
+        self.debut = True
         pass
 
     def reset_debug(self):
         # TODO: Implement the method to reset debug mode
+        self.debug = False
         pass
 
     def read_memory(self, page_number):
@@ -24,12 +31,12 @@ class ClockMMU(MMU):
 
     def get_total_disk_reads(self):
         # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
         # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
         # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
